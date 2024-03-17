c57fb70fe07179f1557adfe6dc3f60549f96bc44
Nirwan Yakub <nirwananshor@gmail.com>
2024-03-17 17:18:39 +0700

    first commit

---
+++ gorm_test.go
@@ -0,0 +1,821 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/logger"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func OpenConnection() *gorm.DB {
+	dialect := "root@tcp(127.0.0.1:3306)/belajar_golang_gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dialect), &gorm.Config{
+		Logger:                 logger.Default.LogMode(logger.Info),
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetConnMaxLifetime(30 * time.Minute)
+	sqlDB.SetConnMaxIdleTime(5 * time.Minute)
+	return db
+}
+
+var db = OpenConnection()
+
+func TestOpenConnection(t *testing.T) {
+	assert.NotNil(t, db)
+}
+
+func TestExecuteSQL(t *testing.T) {
+	err := db.Exec("insert into sample(id, name) values(?,?)", "1", "Nirwan").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values(?,?)", "2", "Budi").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values(?,?)", "3", "Joko").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values(?,?)", "4", "Luli").Error
+	assert.Nil(t, err)
+}
+
+type Sample struct {
+	Id   string
+	Name string
+}
+
+func TestRawSQL(t *testing.T) {
+	var sample Sample
+	err := db.Raw("select id, name from sample where id = ?", "1").Scan(&sample).Error
+	assert.Nil(t, err)
+	assert.Equal(t, "Nirwan", sample.Name)
+
+	var samples []Sample
+	err = db.Raw("select id, name from sample").Scan(&samples).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(samples))
+}
+
+func TestSqlRow(t *testing.T) {
+
+	rows, err := db.Raw("select id, name from sample").Rows()
+	assert.Nil(t, err)
+	defer rows.Close()
+
+	var samples []Sample
+	for rows.Next() {
+		var id string
+		var name string
+
+		err := rows.Scan(&id, &name)
+		assert.Nil(t, err)
+
+		samples = append(samples, Sample{
+			Id:   id,
+			Name: name,
+		})
+	}
+	assert.Equal(t, 4, len(samples))
+}
+
+func TestScanRow(t *testing.T) {
+
+	rows, err := db.Raw("select id, name from sample").Rows()
+	assert.Nil(t, err)
+	defer rows.Close()
+
+	var samples []Sample
+	for rows.Next() {
+		err := db.ScanRows(rows, &samples)
+		assert.Nil(t, err)
+	}
+	assert.Equal(t, 4, len(samples))
+}
+
+func TestCreateUser(t *testing.T) {
+	user := User{
+		ID:       "1",
+		Password: "rahasia",
+		Name: Name{
+			FirstName:  "Nirwan",
+			MiddleName: "Yakub",
+			LastName:   "Setiawan",
+		},
+		Information: "ini akan di ignore",
+	}
+
+	response := db.Create(&user)
+	assert.Nil(t, response.Error)
+	assert.Equal(t, int64(1), response.RowsAffected)
+}
+
+func TestBatchInsert(t *testing.T) {
+	var users []User
+
+	for i := 1; i < 10; i++ {
+		users = append(users, User{
+			ID:       strconv.Itoa(i),
+			Password: "rahasia",
+			Name: Name{
+				FirstName: "User " + strconv.Itoa(i),
+			},
+		})
+	}
+
+	result := db.Create(&users)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 9, int(result.RowsAffected))
+}
+
+func TestTransactionSuccess(t *testing.T) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		err := tx.Create(&User{ID: "10", Password: "rahasia", Name: Name{FirstName: "User 10"}}).Error
+		if err != nil {
+			return err
+		}
+		err = tx.Create(&User{ID: "11", Password: "rahasia", Name: Name{FirstName: "User 11"}}).Error
+		if err != nil {
+			return err
+		}
+		err = tx.Create(&User{ID: "12", Password: "rahasia", Name: Name{FirstName: "User 12"}}).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestTransactionRollback(t *testing.T) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		err := tx.Create(&User{ID: "13", Password: "rahasia", Name: Name{FirstName: "User 13"}}).Error
+		if err != nil {
+			return err
+		}
+		err = tx.Create(&User{ID: "11", Password: "rahasia", Name: Name{FirstName: "User 11"}}).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	assert.NotNil(t, err)
+}
+
+func TestManualTransactionSuccess(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	err := tx.Create(&User{ID: "13", Password: "rahasia", Name: Name{FirstName: "User 13"}}).Error
+	assert.Nil(t, err)
+
+	err = tx.Create(&User{ID: "14", Password: "rahasia", Name: Name{FirstName: "User 14"}}).Error
+	assert.Nil(t, err)
+
+	if err == nil {
+		tx.Commit()
+	}
+}
+
+func TestManualTransactionError(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	err := tx.Create(&User{ID: "15", Password: "rahasia", Name: Name{FirstName: "User 15"}}).Error
+	assert.Nil(t, err)
+
+	err = tx.Create(&User{ID: "14", Password: "rahasia", Name: Name{FirstName: "User 14"}}).Error
+	assert.Nil(t, err)
+
+	if err == nil {
+		tx.Commit()
+	}
+}
+
+func TestQueryAllObjects(t *testing.T) {
+	var users []User
+
+	err := db.Find(&users, "id in ?", []string{"1", "2", "3", "4"}).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(users))
+}
+
+func TestQueryCondition(t *testing.T) {
+	var users []User
+
+	err := db.Where("first_name like ?", "%User%").Where("password = ?", "rahasia").Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 13, len(users))
+}
+
+func TestOrOperator(t *testing.T) {
+	var users []User
+
+	err := db.Where("first_name like ?", "%User%").Or("password = ?", "rahasia").Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 14, len(users))
+}
+
+func TestNotOperator(t *testing.T) {
+	var users []User
+
+	err := db.Not("first_name like ?", "%User%").Where("password = ?", "rahasia").Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(users))
+}
+
+func TestSelectFields(t *testing.T) {
+	var users []User
+
+	err := db.Select("id", "first_name").Find(&users).Error
+	assert.Nil(t, err)
+
+	for _, user := range users {
+		assert.NotNil(t, user.ID)
+		assert.NotEqual(t, "", user.Name.FirstName)
+	}
+
+	assert.Equal(t, 15, len(users))
+}
+
+func TestStructCondition(t *testing.T) {
+	userCondition := User{
+		Name: Name{
+			FirstName: "User 5",
+		},
+		Password: "rahasia",
+	}
+	var users []User
+	err := db.Where(userCondition).Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(users))
+}
+
+func TestMapCondition(t *testing.T) {
+	mapCondition := User{
+		Name: Name{
+			MiddleName: "",
+			LastName:   "",
+		},
+	}
+	var users []User
+	err := db.Where(mapCondition).Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 15, len(users))
+}
+
+func TestOrderLimitOffset(t *testing.T) {
+	var users []User
+	err := db.Order("id asc, first_name desc").Limit(5).Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 5, len(users))
+}
+
+type UserResponse struct {
+	ID        string
+	FirstName string
+	LastName  string
+}
+
+func TestQueryNonModel(t *testing.T) {
+	var users []UserResponse
+	err := db.Model(&User{}).Select("id", "first_name", "last_name").Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 15, len(users))
+	fmt.Println(users)
+}
+
+func TestUpdate(t *testing.T) {
+	user := User{}
+	err := db.Take(&user, "id = ?", "1").Error
+	assert.Nil(t, err)
+
+	user.Name.FirstName = "nirwan"
+	user.Name.MiddleName = ""
+	user.Name.LastName = "yakub"
+	user.Password = "rahasia123"
+
+	err = db.Save(&user).Error
+	assert.Nil(t, err)
+}
+
+func TestUpdateSelectedColumns(t *testing.T) {
+	err := db.Model(&User{}).Where("id = ?", "1").Updates(map[string]interface{}{
+		"middle_name": "",
+		"last_name":   "Morro",
+	}).Error
+	assert.Nil(t, err)
+
+	err = db.Model(&User{}).Where("id = ?", "1").Update("password", "diubahlagi").Error
+	assert.Nil(t, err)
+
+	err = db.Where("id = ?", "1").Updates(User{
+		Name: Name{
+			FirstName: "Nirwan",
+			LastName:  "Yakub",
+		},
+	}).Error
+	assert.Nil(t, err)
+}
+
+func TestAutoIncrement(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		userLog := UserLog{
+			UserId: "1",
+			Action: "Test Action",
+		}
+
+		err := db.Create(&userLog).Error
+		assert.Nil(t, err)
+
+		assert.NotEqual(t, 0, userLog.ID)
+		fmt.Println(userLog.ID)
+	}
+}
+
+func TestSaveOrUpdate(t *testing.T) {
+	userLog := UserLog{
+		UserId: "1",
+		Action: "Test Action",
+	}
+
+	err := db.Save(&userLog).Error
+	assert.Nil(t, err)
+
+	userLog.UserId = "2"
+	err = db.Save(&userLog).Error
+	assert.Nil(t, err)
+}
+
+func TestSaveOrUpdateNonAutoIncrement(t *testing.T) {
+	user := User{
+		ID: "99",
+		Name: Name{
+			FirstName: "User 99",
+		},
+	}
+
+	err := db.Save(&user).Error
+	assert.Nil(t, err)
+
+	user.Name.FirstName = "User 99 Updated"
+	err = db.Save(&user).Error
+	assert.Nil(t, err)
+}
+
+func TestConflict(t *testing.T) {
+	user := User{
+		ID: "88",
+		Name: Name{
+			FirstName: "User 99",
+		},
+	}
+
+	err := db.Clauses(clause.OnConflict{UpdateAll: true}).Create(&user).Error
+	assert.Nil(t, err)
+}
+
+func TestDelete(t *testing.T) {
+	var user User
+	err := db.Take(&user, "id = ?", "88").Error
+	assert.Nil(t, err)
+
+	err = db.Delete(&user).Error
+	assert.Nil(t, err)
+
+	err = db.Delete(&User{}, "id = ?", "99").Error
+	assert.Nil(t, err)
+
+	err = db.Where("id = ?", "77").Delete(&User{}).Error
+	assert.Nil(t, err)
+}
+
+func TestSoftDelete(t *testing.T) {
+	todo := Todo{
+		UserId:      "1",
+		Title:       "Todo 1",
+		Description: "Description 1",
+	}
+	err := db.Create(&todo).Error
+	assert.Nil(t, err)
+
+	err = db.Delete(&todo).Error
+	assert.Nil(t, err)
+	assert.NotNil(t, todo.DeletedAt)
+
+	var todos []Todo
+	err = db.Find(&todos).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(todos))
+}
+
+func TestUnscoped(t *testing.T) {
+	var todo Todo
+	err := db.Unscoped().First(&todo, "id = ?", 1).Error
+	assert.Nil(t, err)
+	fmt.Println(todo)
+
+	err = db.Unscoped().Delete(&todo).Error
+	assert.Nil(t, err)
+
+	var todos []Todo
+	err = db.Unscoped().Find(&todos).Error
+	assert.Nil(t, err)
+}
+
+func TestLock(t *testing.T) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		var user User
+		err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Take(&user, "id = ?", 1).Error
+		if err != nil {
+			return err
+		}
+
+		user.Name.FirstName = "Joko"
+		user.Name.LastName = "Morro"
+		err = tx.Save(&user).Error
+		return err
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateWallet(t *testing.T) {
+	wallet := Wallet{
+		ID:      "1",
+		UserId:  "1",
+		Balance: 1000000,
+	}
+
+	err := db.Create(&wallet).Error
+	assert.Nil(t, err)
+}
+
+func TestRetrieveRelation(t *testing.T) {
+	var user User
+	err := db.Model(&User{}).Preload("Wallet").Take(&user, "id = ?", "1").Error
+	assert.Nil(t, err)
+
+	assert.Equal(t, "1", user.ID)
+	assert.Equal(t, "1", user.Wallet.ID)
+}
+
+func TestRetrieveRelationJoin(t *testing.T) {
+	var user User
+	err := db.Model(&User{}).Joins("Wallet").Take(&user, "users.id = ?", "1").Error
+	assert.Nil(t, err)
+
+	assert.Equal(t, "1", user.ID)
+	assert.Equal(t, "1", user.Wallet.ID)
+}
+
+func TestAutoCreateUpdate(t *testing.T) {
+	user := User{
+		ID:       "20",
+		Password: "rahasia",
+		Name: Name{
+			FirstName: "User 20",
+		},
+		Wallet: Wallet{
+			ID:      "20",
+			UserId:  "20",
+			Balance: 1000000,
+		},
+	}
+
+	err := db.Create(&user).Error
+	assert.Nil(t, err)
+}
+
+func TestSkipAutoCreateUpdate(t *testing.T) {
+	user := User{
+		ID:       "21",
+		Password: "rahasia",
+		Name: Name{
+			FirstName: "User 21",
+		},
+		Wallet: Wallet{
+			ID:      "21",
+			UserId:  "21",
+			Balance: 1000000,
+		},
+	}
+
+	err := db.Omit(clause.Associations).Create(&user).Error
+	assert.Nil(t, err)
+}
+
+func TestUserAndAddresses(t *testing.T) {
+	user := User{
+		ID:       "2",
+		Password: "rahasia",
+		Name: Name{
+			FirstName: "User 2",
+		},
+		Wallet: Wallet{
+			ID:      "2",
+			UserId:  "2",
+			Balance: 1000000,
+		},
+		Addresses: []Address{
+			{
+				UserId:  "2",
+				Address: "Jalan A",
+			},
+			{
+				UserId:  "2",
+				Address: "Jalan B",
+			},
+		},
+	}
+
+	err := db.Create(&user).Error
+	assert.Nil(t, err)
+}
+
+func TestPreloadJoinOneToMany(t *testing.T) {
+	var users []User
+	err := db.Model(&User{}).Preload("Addresses").Joins("Wallet").Find(&users).Error
+	assert.Nil(t, err)
+}
+
+func TestTakePreloadJoinOneToMany(t *testing.T) {
+	var user User
+	err := db.Model(&User{}).Preload("Addresses").Joins("Wallet").Take(&user, "users.id = ?", "50").Error
+	assert.Nil(t, err)
+}
+
+func TestBelongsTo(t *testing.T) {
+	fmt.Println("Preload")
+	var addresses []Address
+	err := db.Model(&Address{}).Preload("User").Find(&addresses).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(addresses))
+
+	fmt.Println("Joins")
+	addresses = []Address{}
+	err = db.Model(&Address{}).Joins("User").Find(&addresses).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(addresses))
+}
+
+func TestBelongsToWallet(t *testing.T) {
+	fmt.Println("Preload")
+	var wallets []Wallet
+	err := db.Model(&Wallet{}).Preload("User").Find(&wallets).Error
+	assert.Nil(t, err)
+
+	fmt.Println("Joins")
+	wallets = []Wallet{}
+	err = db.Model(&Wallet{}).Joins("User").Find(&wallets).Error
+	assert.Nil(t, err)
+}
+
+func TestCreateManyToMany(t *testing.T) {
+	product := Product{
+		ID:    "P001",
+		Name:  "Contoh Product",
+		Price: 1000000,
+	}
+
+	err := db.Create(&product).Error
+	assert.Nil(t, err)
+
+	err = db.Table("user_like_product").Create(map[string]interface{}{
+		"user_id":    "1",
+		"product_id": "P001",
+	}).Error
+	assert.Nil(t, err)
+
+	err = db.Table("user_like_product").Create(map[string]interface{}{
+		"user_id":    "2",
+		"product_id": "P001",
+	}).Error
+	assert.Nil(t, err)
+}
+
+func TestPreloadManyToMany(t *testing.T) {
+	var product Product
+	err := db.Preload("LikeByUsers").Take(&product, "id = ?", "P001").Error
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(product.LikedByUsers))
+}
+
+func TestPreloadManyToManyUser(t *testing.T) {
+	var user User
+	err := db.Preload("LikeProducts").Take(&user, "id = ?", "1").Error
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(user.LikeProducts))
+}
+
+func TestAssociationFind(t *testing.T) {
+	var product Product
+	err := db.Take(&product, "id = ?", "P001").Error
+	assert.Nil(t, err)
+
+	var users []User
+	err = db.Model(&product).Where("users.first_name LIKE ?", "User%").Association("LikedByUsers").Find(&users)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(users))
+}
+
+func TestAssociationAppend(t *testing.T) {
+	var user User
+	err := db.Take(&user, "id = ?", "3").Error
+	assert.Nil(t, err)
+
+	var product Product
+	err = db.Take(&product, "id = ?", "P001").Error
+	assert.Nil(t, err)
+
+	err = db.Model(&product).Association("LikedByUsers").Append(&user)
+	assert.Nil(t, err)
+}
+
+func TestAssociationReplace(t *testing.T) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		var user User
+		err := tx.Take(&user, "id = ?", "1").Error
+		assert.Nil(t, err)
+
+		wallet := Wallet{
+			ID:      "01",
+			UserId:  user.ID,
+			Balance: 1000000,
+		}
+
+		err = tx.Model(&user).Association("Wallet").Replace(&wallet)
+		return err
+	})
+	assert.Nil(t, err)
+}
+
+func TestAssociationDelete(t *testing.T) {
+	var user User
+	err := db.Take(&user, "id = ?", "3").Error
+	assert.Nil(t, err)
+
+	var product Product
+	err = db.Take(&product, "id = ?", "P001").Error
+	assert.Nil(t, err)
+
+	err = db.Model(&product).Association("LikedByUsers").Delete(&user)
+	assert.Nil(t, err)
+}
+
+func TestAssociationClear(t *testing.T) {
+	var user User
+	err := db.Take(&user, "id = ?", "3").Error
+	assert.Nil(t, err)
+
+	var product Product
+	err = db.Take(&product, "id = ?", "P001").Error
+	assert.Nil(t, err)
+
+	err = db.Model(&product).Association("LikedByUsers").Clear()
+	assert.Nil(t, err)
+}
+
+func TestPreloadingWithCondition(t *testing.T) {
+	var user User
+	err := db.Preload("Wallet", "balance > ?", 100).Take(&user, "id = ?", "1").Error
+	assert.Nil(t, err)
+
+	fmt.Println(user)
+}
+
+func TestPreloadingNested(t *testing.T) {
+	var wallet Wallet
+	err := db.Preload("User.Addresses").Take(&wallet, "id = ?", "2").Error
+	assert.Nil(t, err)
+
+	fmt.Println(wallet)
+	fmt.Println(wallet.User)
+	fmt.Println(wallet.User.Addresses)
+}
+
+func TestPreloadingAll(t *testing.T) {
+	var user User
+	err := db.Preload(clause.Associations).Take(&user, "id = ?", "1").Error
+	assert.Nil(t, err)
+}
+
+func TestJoinQuery(t *testing.T) {
+	var users []User
+	err := db.Joins("join wallets on wallets.user_id = users.id").Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(users))
+
+	users = []User{}
+	err = db.Joins("Wallet").Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 18, len(users))
+}
+
+func TestJoinWithCondition(t *testing.T) {
+	var users []User
+	err := db.Joins("join wallets on wallets.user_id = users.id AND wallets.balance > ?", 500000).Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(users))
+
+	users = []User{}
+	err = db.Joins("Wallet").Where("Wallet.balance > ?", 500000).Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(users))
+}
+
+func TestCount(t *testing.T) {
+	var count int64
+	err := db.Model(&User{}).Joins("Wallet").Where("Wallet.balance > ?", 500000).Count(&count).Error
+	assert.Nil(t, err)
+	assert.Equal(t, int64(4), count)
+}
+
+type AggregationResult struct {
+	TotalBalance int64
+	MinBalance   int64
+	MaxBalance   int64
+	AvgBalance   float64
+}
+
+func TestAggregation(t *testing.T) {
+	var result AggregationResult
+	err := db.Model(&Wallet{}).Select("sum(balance) as total_balance", "min(balance) as min_balance", "max(balance) as max_balance", "avg(balance) as avg_balance").Take(&result).Error
+	assert.Nil(t, err)
+
+	assert.Equal(t, int64(4000000), result.TotalBalance)
+	assert.Equal(t, int64(1000000), result.MinBalance)
+	assert.Equal(t, int64(1000000), result.MaxBalance)
+	assert.Equal(t, float64(1000000), result.AvgBalance)
+}
+
+func TestAggregationGroupByAndHaving(t *testing.T) {
+	var results []AggregationResult
+	err := db.Model(&Wallet{}).Select("sum(balance) as total_balance", "min(balance) as min_balance", "max(balance) as max_balance", "avg(balance) as avg_balance").
+		Joins("User").Group("User.id").Having("sum(balance) > ?", 500000).
+		Find(&results).Error
+	assert.Nil(t, err)
+
+	assert.Equal(t, 4, len(results))
+}
+
+func TestContext(t *testing.T) {
+	ctx := context.Background()
+	var users []User
+	err := db.WithContext(ctx).Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 18, len(users))
+}
+
+func BrokeWalletBalance(db *gorm.DB) *gorm.DB {
+	return db.Where("balance = ?", 0)
+}
+
+func SultanWalletBalance(db *gorm.DB) *gorm.DB {
+	return db.Where("balance = ?", 1000000)
+}
+
+func TestScopes(t *testing.T) {
+	var wallets []Wallet
+	err := db.Scopes(BrokeWalletBalance).Find(&wallets).Error
+	assert.Nil(t, err)
+
+	wallets = []Wallet{}
+	err = db.Scopes(SultanWalletBalance).Find(&wallets).Error
+	assert.Nil(t, err)
+}
+
+func TestMigrator(t *testing.T) {
+	err := db.Migrator().AutoMigrate(&GuestBook{})
+	assert.Nil(t, err)
+}
+
+func TestHook(t *testing.T) {
+	user := User{
+		Password: "rahasia",
+		Name: Name{
+			FirstName: "User 100",
+		},
+	}
+	err := db.Create(&user).Error
+	assert.Nil(t, err)
+	assert.NotEqual(t, "", user.ID)
+
+	fmt.Println(user.ID)
+}
---
+++ user.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Name struct {
+	FirstName  string
+	MiddleName string
+	LastName   string
+}
+
+type User struct {
+	ID           string    `gorm:"primary_key;column:id;<-:create"`
+	Password     string    `gorm:"column:password"`
+	Name         Name      `gorm:"embedded"`
+	CreatedAt    time.Time `gorm:"column:created_at;autoCreateTime;<-:create"`
+	UpdatedAt    time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Information  string    `gorm:"-"`
+	Wallet       Wallet    `gorm:"foreignKey:user_id;references:id"`
+	Addresses    []Address `gorm:"foreignKey:user_id;references:id"`
+	LikeProducts []Product `gorm:"many2many:user_like_product;foreignKey:id;joinForeignKey:user_id;references:id;joinReferences:product_id"`
+}
+
+func (u *User) TableName() string {
+	return "users"
+}
+
+func (u *User) BeforeCreate(db *gorm.DB) error {
+	if u.ID == "" {
+		u.ID = "user-" + time.Now().Format("20060102150405")
+	}
+	return nil
+}
+
+type UserLog struct {
+	ID        string `gorm:"primary_key;column:id;autoIncrement"`
+	UserId    string `gorm:"column:user_id"`
+	Action    string `gorm:"column:action"`
+	CreatedAt int64  `gorm:"column:created_at;autoCreateTime;milli"`
+	UpdatedAt int64  `gorm:"column:updated_at;autoCreateTime;milli;autoUpdateTime;milli"`
+}
+
+func (l UserLog) TableName() string {
+	return "user_logs"
+}
